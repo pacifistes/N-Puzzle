e7f0320eb635b7ac4f602d6655a63217f8fb08cf
pacifistes <bbrunell@student.42.fr>
2019-10-20 21:28:52 +0200

    push

--- rust_lib/tests/bench.rs
+++ rust_lib/tests/bench.rs
@@ -1,18 +1,7 @@
 #[cfg(test)]
 mod tests {
     use rust_lib::resolver::generate::*;
-    use rust_lib::resolver::parser::parse;
     use std::time::Instant;
-
-    #[test]
-    fn bench_parser() {
-
-        assert!(parse(&"no_file".to_string()).is_err(), "the function parse should return err");
-        let start = Instant::now();
-        assert!(parse(&"/Users/bbrunell/npuzzle/puzzles/test3".to_string()).is_ok(), "the function parse should return ok");
-        let elapsed = start.elapsed();
-        println!("Time parse the file test : {:?}", elapsed);
-    }
 
     #[test]
     fn bench_r_generate_sorted_state() {
